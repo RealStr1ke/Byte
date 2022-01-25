7555705fa34b9396da166ab8035397cc3b625b45
Thandi Menelas <TMenelas@outlook.com>
2022-01-25 23:46:21 +0000

    Uhh preparing for moderation commands (nice one @Valenciola)

---
+++ src/commands/commands/Moderation/kick.js
@@ -0,0 +1,25 @@
+const Command = require('../../../structs/templates/Command');
+const { MessageEmbed } = require('discord.js');
+const path = require('path');
+class KickCommand extends Command {
+
+	constructor(client) {
+		super(client, {
+			name        : 'kick',
+			description : 'Kicks a member from the current server.',
+			usage       : 'kick <member>',
+			args        : true,
+			directory   : __dirname,
+			userPerms   : 'SEND_MESSAGES',
+            botPerms    : ['SEND_MESSAGES', 'EMBED_LINKS', 'KICK_MEMBERS'],
+			guildOnly   : true,
+			requireData : true,
+		});
+	}
+
+	async run(message, args, data) {
+		
+	}
+}
+
+module.exports = KickCommand;
--- src/config.example.js
+++ src/config.example.js
@@ -1,95 +1,100 @@
-const { Intents, Permissions } = require('discord.js');
+const { Intents, Permissions } = require("discord.js");
 module.exports = {
-	/* The token of the Discord Bot */
-	token: '',
+		/* The token of the Discord Bot */
+		token: '',
+		
+		/* The prefix(s) of the Discord Bot */
+		prefix: 'b!',
 
-	/* The prefix(s) of the Discord Bot */
-	prefix: 'b!',
+		support: {
+			id: '',
+			logs: {
+				database: '',
+				commands: '',
+				errors: '',
+				status: ''
+			}
+		},
+		
+		embed: {
+			color: "#0091fc", // The default color for embeds
+			footer: "Byte | Open Source" // The default footer text for embeds
+		},
 
-	support: {
-		id: '',
-		logs: '',
-	},
+		mongodb: "",
+		
+		/* The information of the bot's owner */
+		owner: {
+			id: '',
+			name: ''
+		},
+	
+		apiKeys: { // The API keys that are used 
+			// AlexFlipnote's API
+			flipnoteAPI: '',
+			// Hypixel API
+			hypixelAPI: '',
+			// Amethyste API (https://api.amethyste.moe)
+			amethyste: ''
+		},
+		status: {
+			name: `with Str1ke's mind`, // The status text of the bot
+			type: 'PLAYING' // The status type of the bot
+		},
 
-	embed: {
-		color: '#0091fc', // The default color for embeds
-		footer: 'Byte | Open Source', // The default footer text for embeds
-	},
+		custom: {
+			/* Custom values go here*/
+		},
+		
+		// Developer Zone
+		/* The debug status of the bot */
+		debug: false,
+		  
+		/* The Discord intents for the bot*/
+		intents: [
+			Intents.FLAGS.GUILDS,
+			Intents.FLAGS.GUILD_MESSAGES,
+			Intents.FLAGS.GUILD_MEMBERS,
+			Intents.FLAGS.GUILD_BANS,
+			Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+			Intents.FLAGS.GUILD_VOICE_STATES,
+			Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS
+		], // or 32767 for All Intents
+		
+		/* The Discord partials for the bot*/
+		partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER'],
 
-	mongodb: '',
-
-	/* The information of the bot's owner */
-	owner: {
-		id: '',
-		name: '',
-	},
-
-	apiKeys: { // The API keys that are used
-		// AlexFlipnote's API
-		flipnoteAPI: '',
-		// Hypixel API
-		hypixelAPI: '',
-		// Amethyste API
-		amethyste: '',
-	},
-	status: {
-		name: 'with Str1ke\'s mind', // The status text of the bot
-		type: 'PLAYING', // The status type of the bot
-	},
-
-	custom: {
-		/* You can put any custom values here in the format of "<variable>: <value>". */
-	},
-
-	// Developer Zone
-	/* The debug status of the bot */
-	debug: false,
-
-	/* The Discord intents for the bot*/
-	intents: [
-		              Intents.FLAGS.GUILDS,
-		              Intents.FLAGS.GUILD_MESSAGES,
-		              Intents.FLAGS.GUILD_MEMBERS,
-		              Intents.FLAGS.GUILD_BANS,
-		              Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-		              Intents.FLAGS.GUILD_VOICE_STATES,
-		              Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-		          ], // or 32767 for All Intents
-
-	/* The Discord partials for the bot*/
-	partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER'],
-
-	/* The Discord permissions for the bot*/
-	permisions: [
-		Permissions.FLAGS.CREATE_INSTANT_INVITE,
-		Permissions.FLAGS.SEND_MESSAGES,
-		Permissions.FLAGS.VIEW_CHANNEL,
-		Permissions.FLAGS.MANAGE_GUILD,
-		Permissions.FLAGS.MANAGE_CHANNELS,
-		Permissions.FLAGS.EMBED_LINKS,
-		Permissions.FLAGS.ATTACH_FILES,
-		Permissions.FLAGS.READ_MESSAGE_HISTORY,
-		Permissions.FLAGS.MENTION_EVERYONE,
-		Permissions.FLAGS.CONNECT,
-		Permissions.FLAGS.SPEAK,
-		Permissions.FLAGS.MUTE_MEMBERS,
-		Permissions.FLAGS.DEAFEN_MEMBERS,
-		Permissions.FLAGS.MOVE_MEMBERS,
-		Permissions.FLAGS.CHANGE_NICKNAME,
-		Permissions.FLAGS.MANANGE_NICKNAMES,
-		Permissions.FLAGS.MANAGE_ROLES,
-		Permissions.FLAGS.MANAGE_EMOJIS_AND_STICKERS,
-		Permissions.FLAGS.USE_APPLICATION_COMMANDS,
-		Permissions.FLAGS.MANAGE_THREADS,
-		Permissions.FLAGS.CREATE_PUBLIC_THREADS,
-		Permissions.FLAGS.CREATE_PRIVATE_THREADS,
-		Permissions.FLAGS.USE_EXTERNAL_STICKERS,
-		Permissions.FLAGS.SEND_MESSAGES_IN_THREADS,
-		Permissions.FLAGS.KICK_MEMBERS,
-		Permissions.FLAGS.BAN_MEMBERS,
-		Permissions.FLAGS.ADD_REACTIONS,
-		Permissions.FLAGS.VIEW_AUDIT_LOG,
-	],
-	scopes: ['bot', 'applications.commands'],
-
+		/* The Discord permissions and scopes for the bot */
+		permisions: [
+			Permissions.FLAGS.CREATE_INSTANT_INVITE,
+			Permissions.FLAGS.SEND_MESSAGES,
+			Permissions.FLAGS.VIEW_CHANNEL,
+			Permissions.FLAGS.MANAGE_GUILD,
+			Permissions.FLAGS.MANAGE_CHANNELS,
+			Permissions.FLAGS.EMBED_LINKS,
+			Permissions.FLAGS.ATTACH_FILES,
+			Permissions.FLAGS.READ_MESSAGE_HISTORY,
+			Permissions.FLAGS.MENTION_EVERYONE,
+			Permissions.FLAGS.CONNECT,
+			Permissions.FLAGS.SPEAK,
+			Permissions.FLAGS.MUTE_MEMBERS,
+			Permissions.FLAGS.DEAFEN_MEMBERS,
+			Permissions.FLAGS.MOVE_MEMBERS,
+			Permissions.FLAGS.CHANGE_NICKNAME,
+			Permissions.FLAGS.MANANGE_NICKNAMES,
+			Permissions.FLAGS.MANAGE_ROLES,
+			Permissions.FLAGS.MANAGE_EMOJIS_AND_STICKERS,
+			Permissions.FLAGS.USE_APPLICATION_COMMANDS,
+			Permissions.FLAGS.MANAGE_THREADS,
+			Permissions.FLAGS.CREATE_PUBLIC_THREADS,
+			Permissions.FLAGS.CREATE_PRIVATE_THREADS,
+			Permissions.FLAGS.USE_EXTERNAL_STICKERS,
+			Permissions.FLAGS.SEND_MESSAGES_IN_THREADS,
+			Permissions.FLAGS.KICK_MEMBERS,
+			Permissions.FLAGS.BAN_MEMBERS,
+			Permissions.FLAGS.ADD_REACTIONS,
+			Permissions.FLAGS.VIEW_AUDIT_LOG
+		],
+		scopes: ['bot', 'applications.commands']
+		
 };
