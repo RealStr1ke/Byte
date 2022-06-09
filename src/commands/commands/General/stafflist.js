12c3b6bf8061f1de5343e91b69712829a22156d7
Thandi Menelas <TMenelas@outlook.com>
2022-06-09 17:04:52 -0400

    Added safeguard for stafflist command

---
+++ src/commands/commands/General/stafflist.js
@@ -0,0 +1,22 @@
+const Command = require('../../../structs/templates/Command');
+const path = require('path');
+
+class StaffListCommand extends Command {
+
+	constructor(client) {
+		super(client, {
+			name        : 'stafflist',
+			description : 'Lists all staff members of the current server',
+			usage       : 'stafflist',
+			args        : false,
+			directory   : __dirname,
+			userPerms   : 'SEND_MESSAGES',
+		});
+	}
+
+	async run(message, args) {
+		return await message.reply('This command hasn\'t been implemented yet');
+	}
+}
+
+module.exports = StaffListCommand;
