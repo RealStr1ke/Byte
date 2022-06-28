65eabba1a705e31047579fabb496316d07b62eb9
Thandi M <TMenelas@outlook.com>
2022-06-28 15:50:23 -0400

    Recreating and organizing existing slash commands

---
+++ src/commands/slash/Animals/bird.js
@@ -0,0 +1,43 @@
+const Slash = require('../../structs/templates/Slash');
+const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const axios = require('axios');
+const path = require('path');
+
+class BirdCommand extends Slash {
+
+	constructor(client) {
+		super(client, {
+			name        : 'bird',
+			description : 'Responds with a random bird picture.',
+			usage       : 'bird',
+			directory   : __dirname,
+			userPerms   : 'SEND_MESSAGES',
+			guildOnly   : false,
+		});
+	}
+
+	async run(interaction) {
+		const response = await axios.get('http://shibe.online/api/birds');
+		const BirdEmbed = new MessageEmbed()
+			.setTitle('**😍 | Awwwww | 😍**')
+			.setImage(response.data[0])
+			.setFooter({
+				text: `Requested by ${interaction.author.tag} • ${this.client.config.embed.footer}`,
+				iconURL: this.client.user.displayAvatarURL(),
+			});
+		return interaction.reply({
+			embeds: [BirdEmbed],
+		});
+	}
+
+	command() {
+		const command = new SlashCommandBuilder()
+			.setName(this.name)
+			.setDescription(this.description)
+			.setDefaultPermission(true);
+		return command;
+	}
+}
+
+module.exports = BirdCommand;
