01c6a8942031e42bee45ede32533fae5cb4aee5c
Thandi M <TMenelas@outlook.com>
2022-08-30 14:36:24 -0400

    Additions to extenders + organization

--- src/modules/helpers/Extenders.js
+++ src/modules/helpers/Extenders.js
@@ -1,10 +1,15 @@
-// const { Message, MessageEmbed, Interaction } = require('discord.js');
-const { Message, MessageEmbed } = require('discord.js');
+const Discord = require('discord.js');
+// const { Message, MessageEmbed } = require('discord.js');
 
+// Message Extenders
 async function AmongUsMessage() {
 	this.reply('Among Us!');
 }
 
+Discord.Message.prototype.sus = AmongUsMessage;
+
+
+// MessageEmbed Extenders
 function DefaultEmbedParams(client) {
 	this
 		.setAuthor({
@@ -18,12 +23,48 @@ function DefaultEmbedParams(client) {
 		.setColor(client.config.embed.color)
 		.setTimestamp();
 	return this;
-}
+	}
+Discord.MessageEmbed.prototype.setDefault = DefaultEmbedParams;
 
+
+// Date Extenders
 function NowUnixTime() {
 	return Math.round(Date.now() / 1000);
 }
 
 Date.prototype.nowUnix = NowUnixTime;
-Message.prototype.sus = AmongUsMessage;
-MessageEmbed.prototype.setDefault = DefaultEmbedParams;
+
+
+// Channel Extenders
+function isText() {
+	return this.type === Discord.ChannelType.GuildText;
+}
+function isVoice() {
+	return this.type === Discord.ChannelType.GuildVoice;
+}
+function isDM() {
+	return this.type === Discord.ChannelType.DM;
+}
+
+Discord.Channel.prototype.isText = isText;
+Discord.Channel.prototype.isVoice = isVoice;
+Discord.Channel.prototype.isDM = isDM;
+
+
+// Interaction Extenders
+function isCommand() {
+	return this.type === Discord.InteractionType.ApplicationCommand;
+}
+function isAutocomplete() {
+	return this.type === Discord.InteractionType.ApplicationCommandAutocomplete;
+}
+function isMessageComponent() {
+	return this.type === Discord.InteractionType.MessageComponent;
+}
+function isModalSubmit() {
+	return this.type === Discord.InteractionType.ModalSubmit;
+}
+Discord.Interaction.prototype.isCommand = isCommand;
+Discord.Interaction.prototype.isAutocomplete = isAutocomplete;
+Discord.Interaction.prototype.isMessageComponent = isMessageComponent;
+Discord.Interaction.prototype.isModalSubmit = isModalSubmit;
